4a7751e0407bbb698bda7186a313602886f60aea
teamitaliaprojexa-dotcom <team.italia.projexa@gmail.com>
2026-08-23 21:42:59 +0200

    modifiche 23 agosto

--- Supporto/CreaDB/settings_clients_add_layout_span.sql
+++
@@ -1,7 +0,0 @@
--- Aggiunge "layout_span" a settings e clients (stessa logica di layout dei progetti):
--- quante colonne occupa il campo nella riga (default 1).
-ALTER TABLE settings ADD COLUMN IF NOT EXISTS layout_span INTEGER DEFAULT 1;
-UPDATE settings SET layout_span = 1 WHERE layout_span IS NULL;
-
-ALTER TABLE clients ADD COLUMN IF NOT EXISTS layout_span INTEGER DEFAULT 1;
-UPDATE clients SET layout_span = 1 WHERE layout_span IS NULL;
