61ba7352e64e2cffb2eca6d72f328d34432fd4ee
teamitaliaprojexa-dotcom <team.italia.projexa@gmail.com>
2026-08-24 12:48:30 +0200

    modifiche 24 agosto

--- Supporto/CreaDB/projects_add_layout_span.sql
+++
@@ -1,4 +0,0 @@
--- Aggiunge "layout_span" a projects: quante colonne occupa il campo nella riga (1, 2 o 3).
--- Default 1 (una colonna). Usato dal layout a righe del flyout progetto.
-ALTER TABLE projects ADD COLUMN IF NOT EXISTS layout_span INTEGER DEFAULT 1;
-UPDATE projects SET layout_span = 1 WHERE layout_span IS NULL;
--- Supporto/CreaDB/valore3_to_numeric2.sql
+++
@@ -1,5 +0,0 @@
--- valore3: da INTEGER a NUMERIC con 2 decimali su settings, clients, projects.
--- I valori interi esistenti si convertono senza perdite.
-ALTER TABLE settings ALTER COLUMN valore3 TYPE NUMERIC(12,2) USING valore3::numeric(12,2);
-ALTER TABLE clients  ALTER COLUMN valore3 TYPE NUMERIC(12,2) USING valore3::numeric(12,2);
-ALTER TABLE projects ALTER COLUMN valore3 TYPE NUMERIC(12,2) USING valore3::numeric(12,2);
